f6694a6fc2b3c63ee20da62e3cec4c245fb5433e
sathya00704 <103351192+sathya00704@users.noreply.github.com>
2023-08-16 00:31:51 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,13 @@
 import 'package:flutter/material.dart';
+import 'pages/home.dart';
+import 'pages/profile.dart';
+import 'pages/loading.dart';
 
-void main()  => runApp(MaterialApp(
-  home: Text('hello'),
-));
+void main() => runApp(MaterialApp(
+  initialRoute: '/',
+    routes:{
+      '/': (context) => Loading(),
+      //'/home': (context) => Home(),
+      //'/profile' : (context) => Profile(),
+    }
+    ));

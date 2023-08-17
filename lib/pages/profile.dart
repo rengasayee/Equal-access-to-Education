10af09f07d24a12f67745e412cc6f4d011da9c19
sathya00704 <103351192+sathya00704@users.noreply.github.com>
2023-08-17 20:05:39 +0530

    Update profile.dart

--- lib/pages/profile.dart
+++ lib/pages/profile.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+//import 'package:url_launcher/url_launcher.dart';
 
-void main()  => runApp(MaterialApp(
-    home: Text('hello'),
+void main() => runApp(MaterialApp(
+  home: Profile(),
 ));
 
 class Profile extends StatefulWidget {
@@ -15,7 +16,257 @@ class _ProfileState extends State<Profile> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.green,
+      backgroundColor: Color(0xFF213555),
+      appBar: AppBar(
+        backgroundColor: Color(0xFF213555),
+        title: Text('My Profile'),
+        centerTitle: true,
+        elevation: 0,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.fromLTRB(15, 0, 0, 0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Text(
+                    'Name: ',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 24,
+                    ),
+                  ),
+                  SizedBox(width: 10),
+                  Text(
+                    'Your Name',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 24,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            //SizedBox(height: 10),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(15, 0, 0, 0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Text(
+                    'Content Language: ',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 24,
+                    ),
+                  ),
+                  SizedBox(width: 10),
+                  Text(
+                    'English',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 24,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            //SizedBox(height: 10),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(15, 0, 0, 0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Text(
+                    'Class Selected: Class',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 24,
+                    ),
+                  ),
+                  SizedBox(width: 10),
+                  Text(
+                    '12',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 24,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 5),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(15, 0, 0, 0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Text(
+                    'Course Completion Percentage',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 24,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 2),
+            Container(
+              height: 80.0,
+              width: 250.0,
+              color: Colors.transparent,
+              child: Container(
+                  decoration: BoxDecoration(
+                      color: Color(0xFFF5EFE7),
+                      borderRadius: BorderRadius.all(Radius.circular(10.0))),
+                  child: new Center(
+                    child: new Text("Progress Bar",
+                      style: TextStyle(fontSize: 22, color: Colors.black),
+                      textAlign: TextAlign.center,),
+                  )),
+            ),
+            //SizedBox(height: 10),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(15, 0, 0, 0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Text(
+                    'Assessment Improvement Rate',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 24,
+                    ),
+                  ),
+                  SizedBox(width: 10),
+                ],
+              ),
+            ),
+            SizedBox(height: 10),
+            Container(
+              height: 150.0,
+              width: 250.0,
+              color: Colors.transparent,
+              child: Container(
+                  decoration: BoxDecoration(
+                      color: Color(0xFFF5EFE7),
+                      borderRadius: BorderRadius.all(Radius.circular(10.0))),
+                  child: new Center(
+                    child: new Text("Graph",
+                      style: TextStyle(fontSize: 22, color: Colors.black),
+                      textAlign: TextAlign.center,),
+                  )),
+            ),
+            SizedBox(height: 10),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(15, 0, 0, 0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Text(
+                    'Learning Reminders',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 24,
+                    ),
+                  ),
+                  SizedBox(width: 110),
+                  TextButton.icon(
+                      onPressed: ()  {
+                      },
+                      icon: Icon(Icons.keyboard_arrow_right,
+                        color: Colors.white,
+                        size: 30,),
+                      label: Text('')
+                  ),
+                ],
+              ),
+            ),
+            //SizedBox(height: 2),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(15, 0, 0, 0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Text(
+                    'Suggestions/Complaints',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 24,
+                    ),
+                  ),
+                  SizedBox(width: 60),
+                  TextButton.icon(
+                      onPressed: ()  {
+                        // const url = 'https://forms.gle/RzeYsWApt74v468u9';
+                        // launch(url);
+                      },
+                      icon: Icon(Icons.keyboard_arrow_right,
+                        color: Colors.white,
+                        size: 30,),
+                      label: Text('')
+                  ),
+                ],
+              ),
+            ),
+            //SizedBox(height: 5),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(15, 0, 0, 0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Text(
+                    'Share the EduBridge App',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 24,
+                    ),
+                  ),
+                  SizedBox(width: 60),
+                  TextButton.icon(
+                      onPressed: ()  {
+                      },
+                      icon: Icon(Icons.keyboard_arrow_right,
+                        color: Colors.white,
+                        size: 30,),
+                      label: Text('')
+                  ),
+                ],
+              ),
+            ),
+            //SizedBox(height: 10),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(15, 0, 0, 0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Text(
+                    'Delete this profile',
+                    style: TextStyle(
+                      color: Colors.redAccent,
+                      fontSize: 24,
+                    ),
+                  ),
+                  SizedBox(width: 140),
+                  TextButton.icon(
+                      onPressed: ()  {
+                      },
+                      icon: Icon(Icons.keyboard_arrow_right,
+                        color: Colors.white,
+                        size: 30,),
+                      label: Text('')
+                  ),
+                ],
+              ),
+            ),
+            //SizedBox(height: 10),
+          ],
+        ),
+      ),
     );
   }
 }
